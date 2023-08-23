1e427815494d5041f768d223368ae1df2b5e8c47
mihkriss <74809078+mihkriss@users.noreply.github.com>
2023-08-24 01:30:57 +0300

    1111

---
+++ app/src/main/java/com/example/infoapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.infoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.infoapp.ui.theme.InfoAppTheme
+import com.example.infoapp.ui_componets.InfoScreen
+import com.example.infoapp.ui_componets.MainScreen
+import com.example.infoapp.utils.ListItem
+import com.example.infoapp.utils.Routes
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val navController = rememberNavController()
+            var item: ListItem? = null
+            InfoAppTheme {
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.MAIN_SCREEN
+                ){
+                    composable(Routes.MAIN_SCREEN){
+                        MainScreen{ listItem ->
+                            item = listItem
+                            navController.navigate(Routes.INFO_SCREEN)
+                        }
+                    }
+
+                    composable(Routes.INFO_SCREEN){
+                        InfoScreen(item = item!!)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/infoapp/MainViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.infoapp
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.infoapp.db.MainDb
+import com.example.infoapp.utils.ListItem
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    val mainDb: MainDb
+): ViewModel() {
+    val mainList = mutableStateOf(emptyList<ListItem>())
+    private var job: Job? = null
+
+    fun getAllItemsByCategory(cat: String) {
+        job?.cancel()
+        job = viewModelScope.launch {
+            mainDb.dao.getAllItemsByCategory(cat).collect{
+                list->mainList.value = list
+            }
+        }
+    }
+
+    fun getFavorites() = viewModelScope.launch {
+        job?.cancel()
+        job = viewModelScope.launch {
+            mainDb.dao.getFavorites().collect{
+                    list->mainList.value = list
+            }
+
+        }
+    }
+
+    fun insertItem(item: ListItem) = viewModelScope.launch {
+        mainDb.dao.insertItem(item)
+    }
+
+    fun deleteItem(item: ListItem) = viewModelScope.launch {
+        mainDb.dao.deleteItem(item)
+    }
+}
